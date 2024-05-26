b89ebb8994f422529edfea6d2da2f484c3c0af7a
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-05-26 10:35:23 +0600

    demo object: created in index.js for backend... successfully

--- user-management-server/index.js
+++ user-management-server/index.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+    { id: 1, name: "one", banglish: "uwan" },
+    { id: 2, name: "two", banglish: "tu" },
+    { id: 3, name: "three", banglish: "there" }
+]
+
 app.get("/", (req, res) => {
     res.send("Hello, User Management Server is running.");
+})
+
+app.get("/users", (req, res) => {
+    res.send(users);
 })
 
 app.listen(port, () => {
