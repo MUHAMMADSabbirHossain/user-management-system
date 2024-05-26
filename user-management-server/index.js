4502a8b6ae3ad8b305544f08f14d0eb208d91ed9
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-05-26 10:29:12 +0600

    boilerplate: added in index.js for backend... successfully

--- user-management-server/index.js
+++ user-management-server/index.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+    res.send("Hello, User Management Server is running.");
+})
+
+app.listen(port, () => {
+    console.log(`User Management Server is running on PORT: ${port}`);
+});
